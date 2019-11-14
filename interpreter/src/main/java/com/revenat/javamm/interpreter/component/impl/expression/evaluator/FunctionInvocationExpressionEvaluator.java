d84bda50d1357cd85e0695bb1e2b950561f59983
Vitaliy Dragun <vdrag00n90@gmail.com>
2019-11-14 19:46:03 +0200

    feat(FunctionInvocationExpressionEvaluator):

---
+++ interpreter/src/main/java/com/revenat/javamm/interpreter/component/impl/expression/evaluator/FunctionInvocationExpressionEvaluator.java
@@ -0,0 +1,46 @@
+
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.revenat.javamm.interpreter.component.impl.expression.evaluator;
+
+import com.revenat.javamm.code.fragment.expression.FunctionInvocationExpression;
+import com.revenat.javamm.interpreter.component.ExpressionEvaluator;
+import com.revenat.javamm.interpreter.component.FunctionInvoker;
+
+import static com.revenat.javamm.interpreter.model.CurrentRuntimeProvider.getCurrentRuntime;
+
+/**
+ * @author Vitaliy Dragun
+ *
+ */
+public class FunctionInvocationExpressionEvaluator implements ExpressionEvaluator<FunctionInvocationExpression> {
+
+    @Override
+    public Class<FunctionInvocationExpression> getExpressionClass() {
+        return FunctionInvocationExpression.class;
+    }
+
+    @Override
+    public Object evaluate(final FunctionInvocationExpression expression) {
+        final FunctionInvoker functionInvoker = getFunctionInnvoker();
+        return functionInvoker.invoke(expression.getFunctionName(), expression.getArguments());
+    }
+
+    private FunctionInvoker getFunctionInnvoker() {
+        return getCurrentRuntime().getCurrentFunctionInvoker();
+    }
+}

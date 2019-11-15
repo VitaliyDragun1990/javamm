181707c34a1932cc8931ad6c0c2c58a1b9f9e487
Vitaliy Dragun <vdrag00n90@gmail.com>
2019-11-15 17:53:44 +0200

    feat(ReturnOperation): add support for 'return' operation to compiler

---
+++ compiler/src/main/java/com/revenat/javamm/compiler/component/impl/operation/simple/ReturnOperationReader.java
@@ -0,0 +1,69 @@
+
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.revenat.javamm.compiler.component.impl.operation.simple;
+
+import com.revenat.javamm.code.fragment.Expression;
+import com.revenat.javamm.code.fragment.SourceLine;
+import com.revenat.javamm.code.fragment.operation.ReturnOperation;
+import com.revenat.javamm.compiler.component.ExpressionResolver;
+import com.revenat.javamm.compiler.component.impl.operation.AbstractOperationReader;
+
+import java.util.ListIterator;
+import java.util.Optional;
+
+import static com.revenat.javamm.code.syntax.Keywords.RETURN;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * @author Vitaliy Dragun
+ *
+ */
+public class ReturnOperationReader extends AbstractOperationReader<ReturnOperation> {
+
+    private final ExpressionResolver expressionResolver;
+
+    public ReturnOperationReader(final ExpressionResolver expressionResolver) {
+        this.expressionResolver = requireNonNull(expressionResolver);
+    }
+
+    @Override
+    protected Optional<String> getOperationDefiningKeyword() {
+        return Optional.of(RETURN);
+    }
+
+    @Override
+    protected ReturnOperation get(final SourceLine sourceLine, final ListIterator<SourceLine> codeIterator) {
+        return buildReturnOperation(sourceLine);
+    }
+
+    private ReturnOperation buildReturnOperation(final SourceLine sourceLine) {
+        if (isExpressionAbsent(sourceLine)) {
+            return new ReturnOperation(sourceLine);
+        }
+        return new ReturnOperation(getExpression(sourceLine), sourceLine);
+    }
+
+    private boolean isExpressionAbsent(final SourceLine sourceLine) {
+        return sourceLine.getTokenCount() == 1;
+    }
+
+    private Expression getExpression(final SourceLine sourceLine) {
+        return expressionResolver.resolve(sourceLine.subList(1), sourceLine);
+    }
+}
---
+++ compiler/src/test/java/com/revenat/javamm/compiler/integration/operation/ReturnOperationReader_Expected_Success_IntegrationTest.java
@@ -0,0 +1,60 @@
+
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.revenat.javamm.compiler.integration.operation;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import com.revenat.javamm.code.fragment.Operation;
+import com.revenat.javamm.code.fragment.operation.ReturnOperation;
+
+import java.util.stream.Stream;
+
+import static java.util.List.of;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+/**
+ * @author Vitaliy Dragun
+ *
+ */
+public class ReturnOperationReader_Expected_Success_IntegrationTest
+        extends AbstractOperationReaderHappyPathIntegrationTest {
+
+    @Override
+    protected Class<? extends Operation> getExpectedOperationClass() {
+        return ReturnOperation.class;
+    }
+
+    @Override
+    protected Stream<Arguments> validSourceLineProvider() {
+        return Stream.of(
+                arguments(of(
+                        "return"
+                )),
+                arguments(of(
+                        "return null"
+                )),
+                arguments(of(
+                        "return 1 + 2 * 6 - a"
+                )),
+                arguments(of(
+                        "return sum (1, a + 3) - sum (4, 5 + 7 * b)"
+                ))
+        );
+    }
+}
